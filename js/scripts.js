51918c0364b341415aa802486eb26ac75de1088e
dimitrie <Bender@Benders-MacBook-Pro.local>
2014-05-29 17:02:14 +0200

    updates
    top menu, behaviour, added mixitup, etc

--- js/scripts.js
+++ js/scripts.js
@@ -9,16 +9,38 @@ function getRandomInt (min, max) {
 $( document ).ready(function() {
     console.log( "ready!" );
 
+    $("#locationmarker").popover({ html : true });
+
     var fruit = $(".fr_div");
     for (var i = 0; i < 3; i++) {
       $(fruit).clone().insertAfter(".fr_div");
     }
+
+    $('.fr_div').each(function(index){
+        $(this).addClass("category-" + (index % 2 === 0 ? 1 : 2));
+    });
 
     var imgs = $(".fr_item").children().children().children("img");
     $.each(imgs, function (index) {
     	$(this).attr('src', 'assets/fullbleed/0'+(index+1)+'.jpg');
     });
 
+    $(function(){
+        $('#container').mixItUp({
+         animation: {
+            perspectiveDistance: '1000px',
+            perspectiveOrigin: '0 50%',
+            easing: 'cubic-bezier(0.375, 0.885, 0.32, 1.275)',
+            enable: true,
+            duration: 200,
+            effects: 'fade stagger(100ms)',
+            staggerSequence: function(i){ return i%3; }
+            }
+        });
+    });
+
+    
+    
 });
 
 
